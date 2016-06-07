5cfd0a4f8e11ef7bfd442575768ef0a58dcc3391
panjan <panjan@panjan.cz>
2016-06-07 16:26:58 +0200

    fix: search

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,11 +2,16 @@ require 'travis/testing/factories'
 
 FactoryGirl.define do
   factory :test_step do
-    description "Test step"
+    description 'Test step'
   end
 
   factory :test_case do
-    description "Test case"
+    description 'Test case'
+  end
+
+  factory :build_customized, parent: :build do
+    sequence(:name) { |n| "foo #{n} bar baz qux quux #{n}" }
+    sequence(:build_info) { |n| "qux #{n} description" }
+    sequence(:state) { |n| (n % 3 == 0) ? 'created' : 'passed' }
   end
 end
-
--- spec/features/lib/final-api_spec.rb
+++ spec/features/lib/final-api_spec.rb
@@ -5,48 +5,48 @@ describe FinalAPI do
   describe '#custom_sum_results' do
     context 'when test contains errored state' do
       it 'returns errored' do
-        expect(FinalAPI.send(:custom_sum_results, { error: 1 })).to eq 'errored'
+        expect(FinalAPI.send(:custom_sum_results, { error: 1 })).to eq 'Errored'
         expect(FinalAPI.send(
           :custom_sum_results,
           { errored: 1, failed: 4, passed: 3, created: 1 }
-        )).to eq 'errored'
+        )).to eq 'Errored'
       end
     end
 
     context 'when test contains failed and no errored' do
       it 'returns errored' do
-        expect(FinalAPI.send(:custom_sum_results, { failed: 1 })).to eq 'failed'
+        expect(FinalAPI.send(:custom_sum_results, { failed: 1 })).to eq 'Failed'
         expect(FinalAPI.send(
           :custom_sum_results,
           { failed: 4, passed: 3, pending: 4, created: 1 }
-        )).to eq 'failed'
+        )).to eq 'Failed'
       end
     end
 
     context 'when no failed, errored and created or pending exists' do
       it 'returns pending' do
-        expect(FinalAPI.send(:custom_sum_results, { created: 1 })).to eq 'created'
-        expect(FinalAPI.send(:custom_sum_results, { pending: 1 })).to eq 'created'
+        expect(FinalAPI.send(:custom_sum_results, { created: 1 })).to eq 'Created'
+        expect(FinalAPI.send(:custom_sum_results, { pending: 1 })).to eq 'Created'
         expect(FinalAPI.send(
           :custom_sum_results,
           { passed: 1, skipped: 1, pending: 1, knownBug: 1 }
-        )).to eq 'created'
+        )).to eq 'Created'
       end
     end
 
     context 'when no failed, errored and one pased' do
       it 'return passed when only skipped, notPerformed knownBug are present' do
-        expect(FinalAPI.send(:custom_sum_results, { passed: 1 })).to eq 'passed'
+        expect(FinalAPI.send(:custom_sum_results, { passed: 1 })).to eq 'Passed'
         expect(FinalAPI.send(
           :custom_sum_results,
           { passed: 1, skipped: 1, notPerformed: 1, knownBug: 1 }
-        )).to eq 'passed'
+        )).to eq 'Passed'
       end
     end
 
     context 'when unknown state' do
       it 'returns errored' do
-        expect(FinalAPI.send(:custom_sum_results, { xxx: 1 })).to eq 'errored'
+        expect(FinalAPI.send(:custom_sum_results, { xxx: 1 })).to eq 'Errored'
       end
     end
   end
