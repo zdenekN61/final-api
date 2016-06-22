63cd8a8b168519f13b11996c68aa8107cf5420f7
Jan Travnicek <trawais002@gmail.com>
2016-06-22 10:15:12 +0200

    Revert "feature: search"

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,16 +2,11 @@ require 'travis/testing/factories'
 
 FactoryGirl.define do
   factory :test_step do
-    description 'Test step'
+    description "Test step"
   end
 
   factory :test_case do
-    description 'Test case'
-  end
-
-  factory :build_customized, parent: :build do
-    sequence(:name) { |n| "foo #{n} bar baz qux quux #{n}" }
-    sequence(:build_info) { |n| "qux #{n} description" }
-    sequence(:state) { |n| (n % 3 == 0) ? 'created' : 'passed' }
+    description "Test case"
   end
 end
+
